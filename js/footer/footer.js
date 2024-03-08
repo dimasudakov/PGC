692e8fa001f21495452a3bf94d3a7401d3c02ed8
AndreyBeloborodov <90422147+AndreyBeloborodov@users.noreply.github.com>
2024-03-08 13:00:39 +0300

    add slide 3 5 and footer

--- js/footer/footer.js
+++ js/footer/footer.js
@@ -13,7 +13,7 @@ const footerContent = `
         <div class="map-wrapper">
             <img src="assets/footer/map-wrapper.png" alt="" class="stroke">
             <div class="map-container">
-                <iframe class="yandex-map" src="https://yandex.ru/map-widget/v1/?um=constructor%3A2985f6f0724324f8077d7bd49ac30c6f5c2e96a902c3b537c88db2f689d77e95&amp;source=constructor"></iframe>
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A71f91eaad6eda709e303855dbf21ec0bb2ee9fed52252e28b51b84ba2e650605&amp;source=constructor" width="640" height="497" frameborder="0"></iframe>
             </div>
         </div>
         <div class="info">
@@ -36,77 +36,77 @@ const footerContent = `
                         <div class="logo-wrapper">
                             <img src="assets/footer/logo-email.png" alt="" class="logo abs-center" />
                         </div>
-                        <div class="f-mx1">htr.conf@gmail.com</div>
+                        <div class="f-mx1">pgc.conf@gmail.com</div>
                     </li>
                     <li>
                         <div class="logo-wrapper">
                             <img src="assets/footer/logo-tg.png" alt="" class="logo abs-center" />
                         </div>
-                        <a href="https://t.me/htr_conf">
-                            <div class="f-mx1">@htr_conf</div>
+                        <a href="https://t.me/pgc_conf">
+                            <div class="f-mx1">@pgc_conf</div>
                         </a>
                     </li>
                 </ul>
             </div>
             <div class="location">
                 <div class="title f-2" id="footer_location-title">Место проведения</div>
-                <h4 id="footer_location" class="f-mx1">Альметьевский государственный нефтяной институт,
-                    Республика Татарстан, г. Альметьевск, ул. Советская, 212к2</h4>
+                <h4 id="footer_location" class="f-mx0_9">
+                <span class="title-blob" id="footer_location-bold"></span>,
+                <span id="footer_location-normal"></span>
+            </h4>
             </div>
         </div>
 `
 
 const footerContentMobile = `
-        <img class="back-mobile-1" src="assets/footer/mobile-c-1.svg" alt="">
-        <img class="back-mobile-2" src="assets/footer/mobile-c-2.svg" alt="">
-        <img class="back-mobile-3" src="assets/footer/mobile-c-3.svg" alt="">
-        <img class="back-mobile-4" src="assets/footer/mobile-c-4.svg" alt="">
-        <img class="back-mobile-5" src="assets/footer/mobile-c-5.svg" alt="">
-        <img class="back-mobile-6" src="assets/footer/mobile-c-6.svg" alt="">
-        <img class="back-mobile-7" src="assets/footer/mobile-c-7.svg" alt="">
-        <div class="contacts">
-            <div class="title f-0_8" id="footer_contacts-title"></div>
-            <ul>
-                <li>
-                    <div class="logo-wrapper">
-                        <img src="assets/footer/logo-user.png" alt="" class="logo abs-center" />
-                    </div>
-                    <div id="footer-person" class="f-mx0_6 a"></div>
-                </li>
-                <li>
-                    <div class="logo-wrapper">
-                        <img src="assets/footer/logo-call.png" alt="" class="logo abs-center" />
-                    </div>
-                    <div id="footer-phone" class="f-mx1 a"></div>
-                </li>
-                <li>
-                    <div class="logo-wrapper">
-                        <img src="assets/footer/logo-email.png" alt="" class="logo abs-center" />
-                    </div>
-                    <div class="f-mx1">htr.conf@gmail.com</div>
-                </li>
-                <li>
-                    <div class="logo-wrapper">
-                        <img src="assets/footer/logo-tg.png" alt="" class="logo abs-center" />
-                    </div>
-                    <a href="https://t.me/htr_conf">
-                        <div class="f-mx1">@htr_conf</div>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="bottom">
+        <img class="back-mobile-1" src="assets/footer/c-1.svg" alt="">
+        <img class="back-mobile-3" src="assets/footer/c-3.svg" alt="">
+        <img class="back-mobile-2" src="assets/footer/c-8.svg" alt="">
+        <img class="back-mobile-4" src="assets/footer/c-5.svg" alt="">
+        <div class="info">
+            <div class="contacts">
+                <div class="title f-2" id="footer_contacts-title"></div>
+                <ul>
+                    <li>
+                        <div class="logo-wrapper">
+                            <img src="assets/footer/logo-user.png" alt="" class="logo abs-center" />
+                        </div>
+                        <div id="footer-person" class="contacts-person f-mx0_9"></div>
+                    </li>
+                    <li>
+                        <div class="logo-wrapper">
+                            <img src="assets/footer/logo-email.png" alt="" class="logo abs-center" />
+                        </div>
+                        <div class="f-mx1">pdc.conf@gmail.com</div>
+                    </li>
+                    <li>
+                        <div class="logo-wrapper">
+                            <img src="assets/footer/logo-call.png" alt="" class="logo abs-center" />
+                        </div>
+                        <div id="footer-phone" class="f-mx0_9"></div>
+                    </li>
+                    <li>
+                        <div class="logo-wrapper">
+                            <img src="assets/footer/logo-tg.png" alt="" class="logo abs-center" />
+                        </div>
+                        <a href="https://t.me/pgc_conf">
+                            <div class="f-mx1">@pgc_conf</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
             <div class="location">
-                <div class="title f-0_8" id="footer_location-title"></div>
-                <div class="f-mx0_6" id="footer_location"></div>
-            </div>
-            <div class="map-wrapper">
-                <img src="assets/footer/map-wrapper.png" alt="" class="stroke">
-                <div class="map-container">
-                    <iframe class="yandex-map" src="https://yandex.ru/map-widget/v1/?um=constructor%3A2985f6f0724324f8077d7bd49ac30c6f5c2e96a902c3b537c88db2f689d77e95&amp;source=constructor"></iframe>
-                </div>
+                <div class="title f-2" id="footer_location-title">Место проведения</div>
+                <h4 id="footer_location" class="f-mx0_9">
+                    <span class="title-blob" id="footer_location-bold"></span>,
+                    <span id="footer_location-normal"></span>
+                </h4>
             </div>
         </div>
+        <div id="footer-mobile" class="footer-mobile">
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A71f91eaad6eda709e303855dbf21ec0bb2ee9fed52252e28b51b84ba2e650605&amp;source=constructor" width="100%" height="300" frameborder="0"></iframe>
+        </div>
+     
 `
 
 const footerLimit = 900;
